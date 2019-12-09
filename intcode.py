70741d7bfd7657736f171841160ce47f9bd082ea
fridokus <raxomukus@gmail.com>
2019-12-09 21:13:02 +0100

    done with 5 again

--- intcode.py
+++ intcode.py
@@ -12,13 +12,13 @@ class IntCode(object):
         self.done = False
         self.point = 0
         self.out = 0
-        self.out_changed = False
 
     def execute(self):
+        self.out_changed = False
         while not self.done and not self.out_changed:
+            print(self.prog)
             opcode_and_modes = [0 for i in range(5)] + list(map(int, str(self.prog[self.point]))) # handle missing zeros hack..
             modes = opcode_and_modes[:-2]
-            print(modes)
             opcode = int(''.join([str(i) for i in opcode_and_modes[-2:]]))
             self.point += 1
             self.run_opcode(modes, opcode)
@@ -46,20 +46,52 @@ class IntCode(object):
             self.point += 1
 
         elif opcode == 3:
-            in_1 = self.read_value(modes)
+            in_1 = popleft(self.inputs)
             self.prog[self.prog[self.point]] = in_1
+            self.point += 1
 
         elif opcode == 4:
             self.out = self.read_value(modes)
             self.out_changed = True
-            self.prog[self.prog[self.point]] = in_1
+
+        elif opcode == 5:
+            if self.read_value(modes):
+                self.point = self.read_value(modes)
+            else:
+                self.point += 1
+
+        elif opcode == 6:
+            if not self.read_value(modes):
+                self.point = self.read_value(modes)
+            else:
+                self.point += 1
+
+        elif opcode == 7:
+            in_1 = self.read_value(modes)
+            in_2 = self.read_value(modes)
+            if in_1 < in_2:
+                self.prog[self.prog[self.point]] = 1
+            else:
+                self.prog[self.prog[self.point]] = 0
+            self.point += 1
+
+        elif opcode == 8:
+            in_1 = self.read_value(modes)
+            in_2 = self.read_value(modes)
+            if in_1 == in_2:
+                self.prog[self.prog[self.point]] = 1
+            else:
+                self.prog[self.prog[self.point]] = 0
+            self.point += 1
 
         elif opcode == 99:
+            self.point -= 1
             self.halt()
 
         else:
             print('Unknown opcode ' + opcode)
             self.halt()
+
 
     def halt(self):
         self.done = True
