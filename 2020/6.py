0ca0a33d194d1bbb1c1d10c3afc85fb2e4dcc146
fridokus <raxomukus@gmail.com>
2020-12-07 13:31:17 +0100

    update 6

--- 2020/6.py
+++ 2020/6.py
@@ -5,4 +5,4 @@ with open('6.in') as f:
     groups = f.read().split('\n\n')
 
 print(sum([len(i) for i in [set(i.replace('\n', '')) for i in groups]]))
-print(sum([len(functools.reduce(set.intersection, [set(list(i)) for i in group.split('\n') if i])) for group in groups]))
+print(sum([len(set.intersection(*[set(list(i)) for i in group.split('\n') if i])) for group in groups]))
