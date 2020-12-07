631114e4311fa8e92f3c98bc6b417ef49772e3e6
fridokus <raxomukus@gmail.com>
2020-12-07 13:25:44 +0100

    Update 6.py to use one line for each solution

--- 2020/6.py
+++ 2020/6.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
+import functools
 
 with open('6.in') as f:
     groups = f.read().split('\n\n')
 
-unions = [set(i.replace('\n', '')) for i in groups]
-
-print(sum([len(i) for i in unions]))
-
-res2 = 0
-for i, group in enumerate(groups):
-    intersection = unions[i]
-    for individual in group.split('\n'):
-        intersection = intersection & set(list(individual)) if individual else intersection
-    res2 += len(intersection)
-
-print(res2)
-
+print(sum([len(i) for i in [set(i.replace('\n', '')) for i in groups]]))
+print(sum([len(functools.reduce(set.intersection, [set(list(i)) for i in group.split('\n') if i])) for group in groups]))
