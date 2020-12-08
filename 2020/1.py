045d6c852ffe5d00c3421eb9ffb8c19ee0c116d2
fridokus <raxomukus@gmail.com>
2020-12-08 08:14:38 +0100

    update 1

--- 2020/1.py
+++ 2020/1.py
@@ -5,5 +5,5 @@ with open('1.in') as f:
     lines = [int(i.strip()) for i in lines]
     l = len(lines)
 
-print(max([max([x * y if x + y == 2020 else 0 for x in lines]) for y in lines]))
-print(max([max([max([x * y * z if x + y + z == 2020 else 0 for x in lines]) for y in lines]) for z in lines]))
+print([x * y for x in lines for y in lines if x + y == 2020][0])
+print([x * y * z for x in lines for y in lines for z in lines if x + y + z == 2020][0])
