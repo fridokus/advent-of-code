a1496b624555cc9c5ee185aacee09990201b2fc4
fridokus <raxomukus@gmail.com>
2020-12-15 11:53:09 +0100

    modify 15

--- 2020/15.py
+++ 2020/15.py
@@ -8,12 +8,8 @@ v = {int(v): k for k, v in enumerate(line[:-1])}
 last_spoken = line[-1]
 for t in range(len(v), 30000000):
     last_added = last_spoken
-    try:
-        last_spoken = t - 1 - v[last_spoken]
-        v[last_added] = t - 1
-    except KeyError:
-        v[last_spoken] = t - 1
-        last_spoken = 0
+    last_spoken = t - 1 - v[last_spoken] if last_spoken in v else 0
+    v[last_added] = t - 1
     if t == 2019: print(last_spoken)
 
 print(last_spoken)
