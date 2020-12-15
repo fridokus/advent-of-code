fdb8e47d6e11ab3b0f1d917033e3c832e6ae0f59
fridokus <raxomukus@gmail.com>
2020-12-15 11:58:04 +0100

    mod 15

--- 2020/15.py
+++ 2020/15.py
@@ -3,13 +3,12 @@
 with open('15.in') as f:
     line = f.read().split(',')
 
-v = {int(v): k for k, v in enumerate(line[:-1])}
-
+v = {int(v): k + 1 for k, v in enumerate(line[:-1])}
 last_spoken = line[-1]
 for t in range(len(v), 30000000):
     last_added = last_spoken
-    last_spoken = t - 1 - v[last_spoken] if last_spoken in v else 0
-    v[last_added] = t - 1
+    last_spoken = t - v[last_spoken] if last_spoken in v else 0
+    v[last_added] = t
     if t == 2019: print(last_spoken)
 
 print(last_spoken)
