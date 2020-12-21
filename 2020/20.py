d8bf808a03c3b2337cbfd5d6a39bd98f9e976160
fridokus <raxomukus@gmail.com>
2020-12-21 15:29:18 +0100

    modify 20 2020

--- 2020/20.py
+++ 2020/20.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-# (7865555081.0, 1091) (3656293393.0, 2347) (5881645369.0, 1459) (3735881843.0, 2297)
-# 2347 is a corner tile, 2053 is next to it (below rotated 180 deg)
-SIZE = 12
 
 tiles = {int(tile[5:9]): tile.splitlines()[1:] for tile in open('20.in').read().split('\n\n')}
-image = [[] for i in range(SIZE)]
-image[0].append((2297, 2))
-# image[0].append((1951, 6))
+image = [[] for i in range(12)]
+image[0].append((2297, 2)) # Did part 1 somewhere else
 
 def flip(image): return [list(i[::-1]) for i in image]
 def rotate(image): return list(zip(*image))[::-1]
 
 def get_image(tile):
-    if tile[1] == 0: return flip(flip(tiles[tile[0]]))
+    if tile[1] == 0: return tiles[tile[0]]
     if tile[1] == 1: return rotate(tiles[tile[0]])
     if tile[1] == 2: return rotate(rotate(tiles[tile[0]]))
     if tile[1] == 3: return rotate(rotate(rotate(tiles[tile[0]])))
@@ -36,13 +32,13 @@ def find_neighbor(tile, direction='right'):
         break
     return (k, o)
 
-for i in range(SIZE):
-    while len(image[i]) != SIZE: image[i].append(find_neighbor(image[i][-1]))
-    if i != SIZE - 1: image[i+1].append(find_neighbor(image[i][0], direction='down'))
+for i in range(12):
+    while len(image[i]) != 12: image[i].append(find_neighbor(image[i][-1]))
+    if i != 12 - 1: image[i+1].append(find_neighbor(image[i][0], direction='down'))
 
-actual_image = [['.' for i in range(SIZE*8)] for j in range(SIZE*8)]
-for j in range(SIZE):
-    for i in range(SIZE):
+actual_image = [['.' for i in range(12*8)] for j in range(12*8)]
+for j in range(12):
+    for i in range(12):
         actual_part = [k[1:-1] for k in get_image(image[j][i])[1:-1]]
         for y in range(8):
             for x in range(8):
@@ -60,8 +56,8 @@ def water_roughness(actual_image):
         for i in row:
             if i == '#': hash_count += 1
     monsters_count = 0
-    for j in range(8*SIZE - monster_h):
-        for i in range(8*SIZE - monster_w):
+    for j in range(8*12 - monster_h):
+        for i in range(8*12 - monster_w):
             monster_here = True
             for y, row in enumerate(monster):
                 for x, pixel in enumerate(row):
