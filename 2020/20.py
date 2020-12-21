315ac5bf82f4a17a4711ce884d45b530b0bf07b5
fridokus <raxomukus@gmail.com>
2020-12-21 15:19:06 +0100

    Done with day 20 :) room for improvement but had enough
    Went surprisingly smooth! no hiccups almost

--- 2020/20.py
+++ 2020/20.py
@@ -5,14 +5,14 @@ SIZE = 12
 
 tiles = {int(tile[5:9]): tile.splitlines()[1:] for tile in open('20.in').read().split('\n\n')}
 image = [[] for i in range(SIZE)]
-# 0 = normal, 1 = 90 degrees, 2 = ... 4 = mirrored, 5 = mirrored 90 degrees, .. 7 = mirrored 270 degrees
 image[0].append((2297, 2))
+# image[0].append((1951, 6))
 
-def flip(image): return [i[::-1] for i in image]
+def flip(image): return [list(i[::-1]) for i in image]
 def rotate(image): return list(zip(*image))[::-1]
 
 def get_image(tile):
-    if tile[1] == 0: return tiles[tile[0]]
+    if tile[1] == 0: return flip(flip(tiles[tile[0]]))
     if tile[1] == 1: return rotate(tiles[tile[0]])
     if tile[1] == 2: return rotate(rotate(tiles[tile[0]]))
     if tile[1] == 3: return rotate(rotate(rotate(tiles[tile[0]])))
@@ -34,11 +34,51 @@ def find_neighbor(tile, direction='right'):
             elif direction == 'down' and all(image[0][i] == pattern[i] for i in range(10)): break
         else: continue
         break
-
-    print(k, o)
     return (k, o)
 
 for i in range(SIZE):
     while len(image[i]) != SIZE: image[i].append(find_neighbor(image[i][-1]))
     if i != SIZE - 1: image[i+1].append(find_neighbor(image[i][0], direction='down'))
 
+actual_image = [['.' for i in range(SIZE*8)] for j in range(SIZE*8)]
+for j in range(SIZE):
+    for i in range(SIZE):
+        actual_part = [k[1:-1] for k in get_image(image[j][i])[1:-1]]
+        for y in range(8):
+            for x in range(8):
+                actual_image[j*8 + y][i*8 + x] = actual_part[y][x]
+
+monster = ['                  # ',
+'#    ##    ##    ###',
+' #  #  #  #  #  #   ']
+monster_w = len(monster[0])
+monster_h = len(monster)
+
+def water_roughness(actual_image):
+    hash_count = 0
+    for row in actual_image:
+        for i in row:
+            if i == '#': hash_count += 1
+    monsters_count = 0
+    for j in range(8*SIZE - monster_h):
+        for i in range(8*SIZE - monster_w):
+            monster_here = True
+            for y, row in enumerate(monster):
+                for x, pixel in enumerate(row):
+                    if pixel == '#' and actual_image[j+y][i+x] != '#':
+                        monster_here = False
+                        break
+            monsters_count += monster_here
+
+    return hash_count - 15*monsters_count
+
+print(min([
+    water_roughness(rotate(rotate(rotate(actual_image)))),
+    water_roughness(rotate(rotate(actual_image))),
+    water_roughness(rotate(actual_image)),
+    water_roughness(actual_image),
+    water_roughness(rotate(rotate(rotate(flip(actual_image))))),
+    water_roughness(rotate(rotate(flip(actual_image)))),
+    water_roughness(rotate(flip(actual_image))),
+    water_roughness(flip(actual_image))
+]))
