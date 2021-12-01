634d03c92dffefd26a25e93674d1262bda6d27a7
Oskar F <raxomukus@gmail.com>
2021-12-01 08:04:18 +0100

    day 1 2021

---
+++ 1.py
@@ -0,0 +1,5 @@
+with open('1.in') as f:
+    lines = [int(i) for i in f]
+
+print(sum([lines[i+1] > lines[i] for i in range(len(lines) - 1)]))
+print(sum([lines[i+3] > lines[i] for i in range(len(lines) - 3)]))
