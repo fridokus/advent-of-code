9d88b68f80a4d3c4cd70b06fdd81c6cac09d7a9f
Oskar F <raxomukus@gmail.com>
2021-12-02 11:39:47 +0100

    Update 2.py

--- 2021/2.py
+++ 2021/2.py
@@ -1,17 +1,18 @@
 with open('2.in') as f:
-    lines = [i.split() for i in f]
+    lines = [i.split() for i in f] 
 
-x = sum([(i[0][0] == 'f') * int(i[1]) for i in lines])
-y = sum([(i[0][0] == 'u') * int(i[1]) - (i[0][0] == 'd') * int(i[1]) for i in lines])
+x = sum([(i[0][0] == 'f') * int(i[1]) for i in lines]) 
+y = sum([(i[0][0] == 'u') * int(i[1]) - (i[0][0] == 'd') * int(i[1]) for i in lines]) 
 
 print(-x*y)
 
-aim = x = y = 0
-for line in lines:
-    if line[0][0] == 'd': aim += int(line[1])
-    elif line[0][0] == 'u': aim -= int(line[1])
-    else:
-        x += int(line[1])
-        y -= aim * int(line[1])
+aim = x = y = 0 
+for word, v in lines:
+    v = int(v)
+    if word[0] == 'd': aim += v
+    elif word[0] == 'u': aim -= v
+    else:   
+        x += v  
+        y -= aim * v 
 
 print(-x*y)
