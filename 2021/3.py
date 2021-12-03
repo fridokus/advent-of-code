06a35611732d71b1a1560107fd06253859162ee5
fridokus <raxomukus@gmail.com>
2021-12-03 10:15:26 +0100

    2021 day 32021 day 32021 day 32021 day 32021 day 32021 day 32021 day 32021 day 32021 day 3

---
+++ 2021/3.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+with open('3.in') as f:
+    lines = f.read().splitlines()
+
+l = len(lines[0])
+sums = [sum([int(line[i]) for line in lines]) for i in range(l)]
+
+gamma = ''.join([str(int(sums[i] >= len(lines)/2)) for i in range(l)])
+epsilon = ''.join([str(int(not int(i))) for i in gamma])
+
+print(int(gamma, 2) * int(epsilon, 2))
+
+def shrink(lines, co):
+    for i in range(l):
+        most_one = sum([int(line[i]) for line in lines]) >= len(lines)/2
+        if most_one != co: lines = [line for line in lines if     int(line[i])]
+        else:              lines = [line for line in lines if not int(line[i])]
+        if len(lines) == 1: return(lines[0])
+
+print(int(shrink(lines, 0), 2) * int(shrink(lines, 1), 2))
