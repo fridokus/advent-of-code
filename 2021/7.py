e2a146c222f76b855a526bc69f8328ae4222ebfd
Oskar F <raxomukus@gmail.com>
2021-12-07 07:55:50 +0100

    day 7

---
+++ 2021/7.py
@@ -0,0 +1,12 @@
+with open('7.in') as f:
+    crabs = [int(i) for i in f.read().split(',')]
+
+n = len(crabs)
+
+crabs = sorted(crabs)
+median = crabs[int(n/2)]
+print(sum([abs(i - median) for i in crabs]))
+
+mean = int(sum(crabs) / n)
+dists = [abs(i - mean) for i in crabs]
+print(sum([i*(i+1)/2 for i in dists]))
