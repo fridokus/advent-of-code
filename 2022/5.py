939a113e5f919b2da80f33a622929c1846ea0952
fridokus <raxomukus@gmail.com>
2022-12-06 14:07:07 +0100

    Update day 5 2022

--- 2022/5.py
+++ 2022/5.py
@@ -7,7 +7,7 @@ with open('5.in') as f:
 
 stacks1 = [[c for c in list(col[-2::-1]) if c != ' '] for col in zip(*stacks1) if col[-1] not in '[ ]']
 
-stacks2 = [[j for j in i ] for i in stacks1]
+stacks2 = [i[:] for i in stacks1]
 for move in moves:
     c, f, t = map(int, re.findall(r'\d+', move))
     for _ in range(c): stacks1[t-1].append(stacks1[f-1].pop())
