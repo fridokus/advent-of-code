89d0309f7c36ad322a3856c205e2a7dc3a2fa6b0
fridokus <raxomukus@gmail.com>
2022-12-09 10:37:59 +0100

    day 9 2022 update

--- 2022/9.py
+++ 2022/9.py
@@ -4,23 +4,10 @@ with open('9.in') as f:
     lines = f.read().splitlines()
 
 directions = {'R': (1, 0), 'L': (-1, 0), 'U': (0, 1), 'D': (0, -1)}
-
-visited = {(0, 0)}
-h, t = [0, 0], [0, 0]
-
 adjacent = lambda p1, p2: all([abs(p1[i] - p2[i]) <= 1 for i in range(2)])
 
-for l in lines:
-    d = directions[l[0]]
-    for _ in range(int(l[2:])):
-        for i in range(2): h[i] += d[i]
-        if not adjacent(h, t):
-            for i in range(2): t[i] += (h[i] != t[i]) * (2*(h[i] > t[i]) - 1)
-            visited.add(tuple(t))
-
-print(len(visited))
-
-visited = {(0, 0)}
+visited1 = {(0, 0)}
+visited9 = {(0, 0)}
 knots = [[0] * 2 for _ in range(10)]
 
 for l in lines:
@@ -31,6 +18,8 @@ for l in lines:
             h, t = knots[k:k+2]
             if not adjacent(h, t):
                 for i in range(2): t[i] += (h[i] != t[i]) * (2*(h[i] > t[i]) - 1)
-        visited.add(tuple(t))
+            if   k == 0: visited1.add(tuple(t))
+            elif k == 8: visited9.add(tuple(t))
 
-print(len(visited))
+print(len(visited1))
+print(len(visited9))
