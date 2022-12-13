1c95342f5de3769b538c4f3da9308eac719833c3
fridokus <raxomukus@gmail.com>
2022-12-13 14:53:49 +0100

    Update day 11 2022

--- 2022/11.py
+++ 2022/11.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-import re
 from functools import reduce
 
 with open('11.in') as f:
@@ -8,58 +7,40 @@ with open('11.in') as f:
 
 class Monkey:
     def __init__(self, op, mod, true_target, false_target, items):
-        self.items = items
         self.op = op
         self.mod = mod
         self.true_target = true_target
         self.false_target = false_target
+        self.items = items
         self.inspected = 0
 
-    def turn(self, div):
-
+    def turn(self, div, monkeys):
         while self.items:
             self.inspected += 1
             old = self.items.pop(0)
-            new = self.op(old)
+            new = eval(self.op)
             if div: new //= 3
-
             new %= global_mod
             if not new % self.mod: monkeys[self.true_target ].items.append(new)
             else:                  monkeys[self.false_target].items.append(new)
 
-monkeys = []
-ops = [lambda x: x*7, lambda x: x+7, lambda x: x*3, lambda x: x+3, lambda x: x*x, lambda x: x+8, lambda x: x+2, lambda x: x+4]
+monkeys  = []
+monkeys2 = []
 for i, m in enumerate(inputs):
-    items = list(map(int, re.findall(r'\d+', m[1])))
-    op = ops[i]
+    items  = eval('[' + m[1][18:] + ']')
+    op = m[2][19:]
     mod = int(m[3][21:])
     true_target = int(m[4][29])
     false_target = int(m[5][30])
-    monkeys.append(Monkey(op, mod, true_target, false_target, items))
+    monkeys .append(Monkey(op, mod, true_target, false_target, items))
+    monkeys2.append(Monkey(op, mod, true_target, false_target, items[:]))
 
-global_mod = reduce(lambda x, y: x*y, [m.mod for m in monkeys])
+mul = lambda x, y: x*y
+global_mod = reduce(mul, [m.mod for m in monkeys])
 for round in range(20):
-    for i, monkey in enumerate(monkeys):
-        monkey.turn(div=True)
-
-l = sorted([m.inspected for m in monkeys])
-r2 = l[-1] * l[-2]
-print(r2)
-
-monkeys = []
-
-for i, m in enumerate(inputs):
-    items = list(map(int, re.findall(r'\d+', m[1])))
-    op = ops[i]
-    mod = int(m[3][21:])
-    true_target = int(m[4][29])
-    false_target = int(m[5][30])
-    monkeys.append(Monkey(op, mod, true_target, false_target, items))
+    for m in monkeys: m.turn(True, monkeys)
+print(mul(*sorted([m.inspected for m in monkeys])[-2:]))
 
 for round in range(10000):
-    for i, monkey in enumerate(monkeys):
-        monkey.turn(div=False)
-
-l = sorted([m.inspected for m in monkeys])
-r2 = l[-1] * l[-2]
-print(r2)
+    for m in monkeys2: m.turn(False, monkeys2)
+print(mul(*sorted([m.inspected for m in monkeys2])[-2:]))
