18468702f4dc818a63c126c169bd785672abb4ee
fridokus <raxomukus@gmail.com>
2024-01-22 15:37:25 +0100

    update 4 2023

--- 2023/4.py
+++ 2023/4.py
@@ -6,20 +6,16 @@ with open('4.in') as f:
     lines = f.read().splitlines()
 
 r1 = 0
-for line in lines:
-    wa = line[10:].split('|')
-    w, a = [set(map(int, re.findall(r'\d+', i))) for i in wa]
-    p = 0
-    for i in a:
-        if i in w: p = max(p*2, 1)
-    r1 += p
-print(r1)
-
 c = [1 for i in range(len(lines))]
 for j, line in enumerate(lines):
     wa = line[10:].split('|')
     w, a = [set(map(int, re.findall(r'\d+', i))) for i in wa]
-    p = 0
-    for i in a: p += i in w
-    for i in range(j+1, j+p+1): c[i] += c[j]
+    p1 = p2 = 0
+    for i in a:
+        if i in w:
+            p1 = max(p1*2, 1)
+            p2 += 1
+    for i in range(j+1, j+p2+1): c[i] += c[j]
+    r1 += p1
+print(r1)
 print(sum(c))
