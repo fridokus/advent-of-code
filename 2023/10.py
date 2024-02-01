b089d6ab4c543ec782275f8bbd83fae6ced60ee4
fridokus <raxomukus@gmail.com>
2024-02-01 16:09:18 +0100

    day 10 2023 done finally
    After a lot of thinking..
    
    Just move along the loop and add all points on one side to an inner loop
    `c`. Then iteratively add neighbors of `c` to `c` (that are not on the
    loop).
    
    Just need to make sure that we add points on the correct side of the
    loop.. :)

--- 2023/10.py
+++ 2023/10.py
@@ -17,24 +17,39 @@ def adjacent(p):
     if m[p] == 'J': return {(p[0]-1, p[1]), (p[0], p[1]-1)}
     if m[p] == '7': return {(p[0]+1, p[1]), (p[0], p[1]-1)}
     if m[p] == 'F': return {(p[0]+1, p[1]), (p[0], p[1]+1)}
-    if m[p] == 'S': return {(p[0]+1, p[1]), (p[0], p[1]+1), (p[0]-1, p[1]), (p[0], p[1]-1)}
+    if m[p] == 'S': return [(p[0]+1, p[1]), (p[0], p[1]+1), (p[0]-1, p[1]), (p[0], p[1]-1)][::-1] # 🤣
     return set()
 
 d = defaultdict(set)
 for j, i in product(range(h), range(w)):
     if m[(j, i)] == 'S':
         d[0] = {(j, i)}
-        visited = {(j, i)}
+        v = {(j, i)}
         break
 
+c = set()
 for i in range(h*w):
     for p in d[i]:
         for a in adjacent(p):
             if a not in d[i-1] and p in adjacent(a):
                 d[i+1] |= {a}
-                visited |= {a}
-    if not d[i+1]: break
-print(i)
+                v |= {a}
+                if   a[0] - p[0] == -1: c |= {(p[0], p[1] + 1)} | {(a[0], a[1] + 1)}
+                elif a[0] - p[0] ==  1: c |= {(p[0], p[1] - 1)} | {(a[0], a[1] - 1)}
+                elif a[1] - p[1] == -1: c |= {(p[0] - 1, p[1])} | {(a[0] - 1, a[1])}
+                elif a[1] - p[1] ==  1: c |= {(p[0] + 1, p[1])} | {(a[0] + 1, a[1])}
+                break
+    if d[0] == d[i+1]: break
+print((i+1) // 2)
 
-for j, i in product(range(h), range(w)):
-    p = (j, i)
+c -= v
+adjacent = lambda p: {(p[0]-1, p[1]), (p[0]+1, p[1]), (p[0], p[1]-1), (p[0], p[1]+1)}
+while True:
+    c2 = set()
+    for p in c:
+        c2 |= {p}
+        for a in adjacent(p):
+            if a not in v: c2 |= {a}
+    if c == c2: break
+    c = c2
+print(len(c))
