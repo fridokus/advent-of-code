7be773708ed3f6fdd5c83b06e346ec16f1f4ef7c
fridokus <raxomukus@gmail.com>
2024-02-05 09:22:23 +0100

    part 1 day 12 2023

--- 2023/12.py
+++ 2023/12.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 
+from re import findall
+
 with open('12.in') as f:
     rows = ((list(l.split()[0]), eval('(' + l.split()[1] + ')')) for l in f.read().splitlines())
 
-for r in rows:
-    pass
+def variations(r):
+    for i in range(len(r)):
+        if r[i] == '?':
+            yield from variations(r[:i] + ['.'] + r[i+1:])
+            yield from variations(r[:i] + ['#'] + r[i+1:])
+            break
+    else: yield r
+
+print(sum([sum([tuple(map(len, findall(r'#+', ''.join(v)))) == r[1] for v in variations(r[0])]) for r in rows]))
