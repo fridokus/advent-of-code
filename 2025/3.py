901a03cb754deaf3f4df4998558fd3ae6b82c91b
oskar F <raxomukus@gmail.com>
2025-12-03 09:32:38 +0000

    clever 3 2025

--- 2025/3.py
+++ 2025/3.py
@@ -1,31 +1,4 @@
 #! /usr/bin/env python3
-with open('3.in', 'r') as f:
-    full_input = f.read().splitlines()
-
-
-def find_highest_joltage(bank: str) -> int:
-    highest_joltage = 0
-    for i in range(len(bank)):
-        for j in range(i + 1, len(bank)):
-            joltage = int(bank[i] + bank[j])
-            if joltage > highest_joltage:
-                highest_joltage = joltage
-    return highest_joltage
-
-
-print(sum(find_highest_joltage(bank) for bank in full_input))
-
-def find_highest_joltage(bank: str, n: int=2) -> int:
-    highest_joltage = ''
-    while len(highest_joltage) < n:
-        max_found, max_found_index = 0, 0
-        for i in range(len(bank) - (n - len(highest_joltage)) + 1):
-            if int(bank[i]) > max_found:
-                max_found = int(bank[i])
-                max_found_index = i
-        highest_joltage += str(max_found)
-        bank = bank[max_found_index + 1:]
-    return int(highest_joltage)
-
-
-print(sum(find_highest_joltage(bank, 12) for bank in full_input))
+data = open('3.in').read().splitlines()
+solve = lambda s, n: n and (m := max(s[:-n+1 or None])) + solve(s[s.find(m)+1:], n-1) or ''
+[print(sum(int(solve(row, k)) for row in data)) for k in (2, 12)]
