283d5cbefb1812ecd11b2d02ebeb12c5e9aa1148
oskar F <raxomukus@gmail.com>
2025-12-11 12:49:37 +0000

    2025 11

--- 2025/11.py
+++ 2025/11.py
@@ -1,5 +1,4 @@
 #! /usr/bin/env python3
-import functools
 
 lines = open('11.in').read().splitlines()
 
@@ -7,8 +6,11 @@ d = dict()
 for line in lines:
     d[line[:3]] = line[5:].split(' ')
 
-@functools.cache
+memo = {}
 def paths(current, target):
+    if (current, target) in memo:
+        return memo[(current, target)]
+
     if current == target:
         return 1
     if current not in d:
@@ -17,11 +19,12 @@ def paths(current, target):
     r = 0
     for o in d[current]:
         r += paths(o, target)
+
+    memo[(current, target)] = r
     return r
 
 r1 = paths('you', 'out')
 print(r1)
 
-# only one path will be valid since acyclic
 r2 = paths('svr', 'fft') * paths('fft', 'dac') * paths('dac', 'out')
 print(r2)
